b0be36db78a7c70f4802971336b4afc46aa952bf
webslavik <slavik.sukhanov@gmail.com>
2019-05-09 18:38:49 +0300

    feat: add Google Auth for login and logout

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,14 +1,18 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-
 import PropTypes from 'prop-types';
 import { withStyles } from '@material-ui/core/styles';
-import AppBar from '@material-ui/core/AppBar';
-import Toolbar from '@material-ui/core/Toolbar';
-// import Typography from '@material-ui/core/Typography';
-import Button from '@material-ui/core/Button';
-import IconButton from '@material-ui/core/IconButton';
+import { GoogleLogin, GoogleLogout } from 'react-google-login';
+import {
+  AppBar,
+  Avatar,
+  Toolbar,
+  Typography,
+  IconButton,
+} from '@material-ui/core';
 import HomeIcon from '@material-ui/icons/Home';
+
+const clientId = '30451489230-rj4nv9du38qfu043v9use2m8ct3b38su.apps.googleusercontent.com';
 
 const styles = {
   root: {
@@ -23,28 +27,95 @@ const styles = {
   },
   linkColor: {
     color: '#ffffff',
+  },
+  userElements: {
+    display: 'flex',
+    alignItems: 'center',
+  },
+  userName: {
+    marginRight: 12,
+  },
+  userAvatar: {
+    marginRight: 16
   }
 };
 
-function NavBar(props) {
-  const { classes } = props;
 
-  return (
-    <AppBar>
-      <Toolbar >
-        <Link to='/news' className={classes.linkColor}>
-          <IconButton className={classes.menuButton} color="inherit" aria-label="Menu">
-            <HomeIcon />
-          </IconButton>
-        </Link>
-        <div className={classes.grow}></div>
-        {/* <Typography variant="h6" color="inherit" className={classes.grow}>
-          News
-        </Typography> */}
-        <Button color="inherit">Sign in</Button>
-      </Toolbar>
-    </AppBar>
-  )
+class NavBar extends React.Component {
+  state = {
+    isLogin: false,
+    userName: null,
+    userAvatar: null,
+  }
+
+  onLogin = response => {
+    const { profileObj } = response;
+
+    this.setState({ 
+      isLogin: true,
+      userName: profileObj.name,
+      userAvatar: profileObj.imageUrl
+    });
+
+    console.log('response:', response);
+  }
+  
+  onLogout = () => {
+    this.setState({ 
+      isLogin: false,
+      userName: null,
+      userAvatar: null,
+    });
+  }
+
+  onError = response => {
+    console.error('error:', response);
+  }
+
+
+  render() {
+    const { classes } = this.props;
+
+    return (
+      <AppBar>
+        <Toolbar >
+          <Link to='/news' className={classes.linkColor}>
+            <IconButton className={classes.menuButton} color="inherit" aria-label="Menu">
+              <HomeIcon />
+            </IconButton>
+          </Link>
+          <div className={classes.grow}></div>
+
+          {!this.state.isLogin &&   
+            <GoogleLogin
+              clientId={clientId}
+              onSuccess={this.onLogin}
+              onFailure={this.onError}>
+              Log in 
+            </GoogleLogin>
+          }
+
+          {this.state.isLogin &&
+            <div className={classes.userElements}>
+              <Typography 
+                variant="subtitle2" 
+                color="inherit" 
+                className={classes.userName}>
+                  {this.state.userName}
+              </Typography>
+              <Avatar 
+                alt="avatar" 
+                src={this.state.userAvatar}
+                className={classes.userAvatar} />
+              <GoogleLogout 
+                buttonText="Log out"
+                onLogoutSuccess={this.onLogout} />
+            </div>
+          }
+        </Toolbar>
+      </AppBar>
+    )
+  }
 }
 
 NavBar.propTypes = {
